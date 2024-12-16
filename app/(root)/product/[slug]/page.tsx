0b4515f7f6cd501053535119cb07c5e6fb2b0cb2
basir <basir.jafarzadeh@gmail.com>
2024-12-16 16:36:29 +0330

    11-Implement-Add-To-Cart

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -12,6 +12,8 @@ import ProductSlider from '@/components/shared/product/product-slider'
 import Rating from '@/components/shared/product/rating'
 import BrowsingHistoryList from '@/components/shared/browsing-history-list'
 import AddToBrowsingHistory from '@/components/shared/product/add-to-browsing-history'
+import AddToCart from '@/components/shared/product/add-to-cart'
+import { generateId, round2 } from '@/lib/utils'
 
 export async function generateMetadata(props: {
   params: Promise<{ slug: string }>
@@ -98,7 +100,6 @@ export default async function ProductDetails(props: {
             <Card>
               <CardContent className='p-4 flex flex-col  gap-4'>
                 <ProductPrice price={product.price} />
-
                 {product.countInStock > 0 && product.countInStock <= 3 && (
                   <div className='text-destructive font-bold'>
                     {`Only ${product.countInStock} left in stock - order soon`}
@@ -108,6 +109,25 @@ export default async function ProductDetails(props: {
                   <div className='text-green-700 text-xl'>In Stock</div>
                 ) : (
                   <div className='text-destructive text-xl'>Out of Stock</div>
+                )}
+                {product.countInStock !== 0 && (
+                  <div className='flex justify-center items-center'>
+                    <AddToCart
+                      item={{
+                        clientId: generateId(),
+                        product: product._id,
+                        countInStock: product.countInStock,
+                        name: product.name,
+                        slug: product.slug,
+                        category: product.category,
+                        price: round2(product.price),
+                        quantity: 1,
+                        image: product.images[0],
+                        size: size || product.sizes[0],
+                        color: color || product.colors[0],
+                      }}
+                    />
+                  </div>
                 )}
               </CardContent>
             </Card>
--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,5 +1,5 @@
-import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
+import CartButton from './cart-button'
 
 export default function Menu() {
   return (
@@ -9,12 +9,7 @@ export default function Menu() {
           Hello, Sign in
         </Link>
 
-        <Link href='/cart' className='header-button'>
-          <div className='flex items-end'>
-            <ShoppingCartIcon className='h-8 w-8' />
-            Cart
-          </div>
-        </Link>
+        <CartButton />
       </nav>
     </div>
   )
