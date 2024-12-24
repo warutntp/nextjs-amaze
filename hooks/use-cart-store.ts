afe44119bf751c43bfb360988e45793e55492a68
basir <basir.jafarzadeh@gmail.com>
2024-12-24 12:50:32 +0330

    17-create-checkout-page

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-import { Cart, OrderItem } from '@/types'
+import { Cart, OrderItem, ShippingAddress } from '@/types'
 import { calcDeliveryDateAndPrice } from '@/lib/actions/order.actions'
 
 const initialState: Cart = {
@@ -11,15 +11,19 @@ const initialState: Cart = {
   shippingPrice: undefined,
   totalPrice: 0,
   paymentMethod: undefined,
+  shippingAddress: undefined,
   deliveryDateIndex: undefined,
 }
 
 interface CartState {
   cart: Cart
   addItem: (item: OrderItem, quantity: number) => Promise<string>
-
   updateItem: (item: OrderItem, quantity: number) => Promise<void>
   removeItem: (item: OrderItem) => void
+  clearCart: () => void
+  setShippingAddress: (shippingAddress: ShippingAddress) => Promise<void>
+  setPaymentMethod: (paymentMethod: string) => void
+  setDeliveryDateIndex: (index: number) => Promise<void>
 }
 
 const useCartStore = create(
@@ -28,7 +32,7 @@ const useCartStore = create(
       cart: initialState,
 
       addItem: async (item: OrderItem, quantity: number) => {
-        const { items } = get().cart
+        const { items, shippingAddress } = get().cart
         const existItem = items.find(
           (x) =>
             x.product === item.product &&
@@ -62,6 +66,7 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calcDeliveryDateAndPrice({
               items: updatedCartItems,
+              shippingAddress,
             })),
           },
         })
@@ -74,7 +79,7 @@ const useCartStore = create(
         )?.clientId!
       },
       updateItem: async (item: OrderItem, quantity: number) => {
-        const { items } = get().cart
+        const { items, shippingAddress } = get().cart
         const exist = items.find(
           (x) =>
             x.product === item.product &&
@@ -95,12 +100,13 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calcDeliveryDateAndPrice({
               items: updatedCartItems,
+              shippingAddress,
             })),
           },
         })
       },
       removeItem: async (item: OrderItem) => {
-        const { items } = get().cart
+        const { items, shippingAddress } = get().cart
         const updatedCartItems = items.filter(
           (x) =>
             x.product !== item.product ||
@@ -113,12 +119,57 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calcDeliveryDateAndPrice({
               items: updatedCartItems,
+              shippingAddress,
             })),
+          },
+        })
+      },
+      setShippingAddress: async (shippingAddress: ShippingAddress) => {
+        const { items } = get().cart
+        set({
+          cart: {
+            ...get().cart,
+            shippingAddress,
+            ...(await calcDeliveryDateAndPrice({
+              items,
+              shippingAddress,
+            })),
+          },
+        })
+      },
+      setPaymentMethod: (paymentMethod: string) => {
+        set({
+          cart: {
+            ...get().cart,
+            paymentMethod,
+          },
+        })
+      },
+      setDeliveryDateIndex: async (index: number) => {
+        const { items, shippingAddress } = get().cart
+
+        set({
+          cart: {
+            ...get().cart,
+            ...(await calcDeliveryDateAndPrice({
+              items,
+              shippingAddress,
+              deliveryDateIndex: index,
+            })),
+          },
+        })
+      },
+      clearCart: () => {
+        set({
+          cart: {
+            ...get().cart,
+            items: [],
           },
         })
       },
       init: () => set({ cart: initialState }),
     }),
+
     {
       name: 'cart-store',
     }
